edd53342d717a156df171311395c90ac24169ec6
Jesus Milan <milan.jesusdev@gmail.com>
2022-04-26 20:46:42 -0400

    using setstate to update the state

--- simple-react-app/src/components/counter.jsx
+++ simple-react-app/src/components/counter.jsx
@@ -5,8 +5,9 @@ class Counter extends Component {
     count: 1,
   };
 
-  handleIncrement = () => {
-    console.log("Increment Clicked!", this);
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
@@ -14,7 +15,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
