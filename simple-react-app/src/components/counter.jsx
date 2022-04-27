5e9a2cc42b0da7f5a95afdbe2ea9eabd1f4c827a
Jesus Milan <milan.jesusdev@gmail.com>
2022-04-26 21:23:55 -0400

    raising and handling events

--- simple-react-app/src/components/counter.jsx
+++ simple-react-app/src/components/counter.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    value: this.props.value,
   };
 
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -20,18 +19,21 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button onClick={this.props.onDelete} className="btn btn-danger btn-sm">
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
